64ce9cf4e748f6555d7313454083cd1d524028d8
Michael Mitrakos <mike.mitrakos@gmail.com>
2024-12-29 18:09:40 +0200

    add canonical

--- src/app/blog/wrapper.jsx
+++ src/app/blog/wrapper.jsx
@@ -8,6 +8,11 @@ import { loadArticles } from '@/lib/mdx'
 
 export default async function BlogArticleWrapper({ article, children }) {
   let allArticles = await loadArticles()
+  if (!allArticles || !Array.isArray(allArticles)) {
+    console.error("No articles found or invalid articles data.");
+    return null; // Return early if articles are missing
+  }
+  
   let moreArticles = allArticles
     .filter(({ metadata }) => metadata !== article)
     .slice(0, 2)
@@ -18,16 +23,16 @@ export default async function BlogArticleWrapper({ article, children }) {
         <FadeIn>
           <header className="mx-auto flex max-w-5xl flex-col text-center">
             <h1 className="mt-6 font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-6xl">
-              {article.title}
+              {article?.title}
             </h1>
             <time
-              dateTime={article.date}
+              dateTime={article?.date}
               className="order-first text-sm text-neutral-950"
             >
-              {formatDate(article.date)}
+              {formatDate(article?.date)}
             </time>
             <p className="mt-6 text-sm font-semibold text-neutral-950">
-              by {article.author.name}, {article.author.role}
+              by {article?.author.name}, {article?.author.role}
             </p>
           </header>
         </FadeIn>
